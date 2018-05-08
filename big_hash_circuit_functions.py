d060c30ea20755ee9940a99f20fdda8886d76a72
timashana <atimash000@citymail.cuny.edu>
2018-05-08 02:21:09 -0400

    Added 256x256 px image together with the modified program files and bitstring

---
+++ big_hash_circuit_functions.py
@@ -0,0 +1,128 @@
+'''
+4/24/2018 Anastasiia Timashova github.com/timashana
+
+FUNCTIONS MODULE CREATING LIBSCAPI-FORMAT CIRCUIT FILE FOR BLOCK MEAN VALUE YAO CIRCUIT
+
+In below code:
+curr_wire last used label in the circuit
+gates current number of gates in the circuit
+l a list of libscapi-format gate declarations for the circuit
+one 1-wire label
+zero 0-wire label
+
+All lists representing binary numbers are enumerated from LSB to MSB.
+That is, A = [A0, A1, ..., An] represents binary number An..A2A1A0
+
+Unless specified, all variables are not actual values, but labels to the corresponding wires
+'''
+
+from math import log
+
+def AND(a, b, c):
+    '''PRE: a, b input labels, c output label
+        POST: libscapy-format string for AND gate with specified labels'''
+    return('2 1 '+str(a)+' '+str(b)+' '+str(c)+' 0001')
+
+def XOR(a, b, c):
+    '''PRE: a, b input labels, c output label
+            POST: libscapy-format string for XOR gate with specified labels'''
+    return('2 1 '+str(a)+' '+str(b)+' '+str(c)+' 0110')
+
+def NOT(a, b):
+    '''PRE: a input label, b output label
+            POST: libscapy-format string for NOT gate with specified labels'''
+    return('1 1 '+str(a)+' '+str(b)+' 10')
+
+def ADD(a, b, c_in, l):
+    '''PRE: a, b labels for the two addends, c_in label for carry-in
+        POST: l contains all gates for the binary full-adder, returned are labels for sum and carry-out'''
+    ladd=[XOR(a, b, c_in + 1), AND(a, b, c_in + 2), XOR(c_in, c_in + 1, c_in + 3), AND(c_in, c_in + 1, c_in + 4), XOR(c_in + 2, c_in + 4, c_in + 5), AND(c_in + 2, c_in + 4, c_in + 6), XOR(c_in + 5, c_in + 6, c_in + 7)]
+    l+=ladd
+    sum, c_out = c_in + 3, c_in + 7
+    return sum, c_out
+
+def TWOCOMPARATOR(a, b, curr_wire, one, l):
+    '''PRE: labels: a minuend, b subtrahend,  one 1-wire
+        POST: l contains all gates for the 2-bit comparator, returned is the label for result (w/values: 1 => A>=B, 0 => A<B)'''
+    lsub=[XOR(b, one, curr_wire + 1), XOR(curr_wire + 1, a, curr_wire + 2), AND(curr_wire + 1, a, curr_wire + 3), AND(curr_wire + 2, curr_wire, curr_wire + 4), XOR(curr_wire + 3, curr_wire + 4, curr_wire + 5), AND(curr_wire + 3, curr_wire + 4, curr_wire + 6), XOR(curr_wire + 5, curr_wire + 6, curr_wire + 7)]
+    l+=lsub
+    l_cout = curr_wire+7
+    return l_cout
+
+def COMPARATOR(A, B, one, curr_wire, gates, l):
+    '''PRE: A, B 8-bit binary lists
+        POST: l contains all gates for the 8-bit comparator, returned are result wire label (w/values: 1 => A>=B, 0 => A<B) and updated gates number'''
+
+    # OR curr_wire and one to make curr_wire carry value of 1
+    l_curr_wire_to_one = [XOR(one, curr_wire, curr_wire+1), AND(one, curr_wire, curr_wire+2), XOR(curr_wire+1, curr_wire+2, curr_wire+3)]
+    l+=l_curr_wire_to_one
+    curr_wire+=3
+    gates+=3
+
+    for i in range(8):
+        curr_wire=TWOCOMPARATOR(A[i], B[i], curr_wire, one, l)
+        gates+=7
+    return curr_wire, gates
+
+def BINADDER(A, B, zero, curr_wire, gates, l):
+    '''PRE: A, B two n-bit binary lists
+        POST: l contains all gates for the n-bit ripple-carry adder, returned are sum wire label, updated curr_wire and gates'''
+
+    # extend A, B by 0-valued bit on the left to account for overflow in the sum
+    A.append(zero)
+    B.append(zero)
+    size=len(A)
+    # use half-adder for the LSB's (no carry-in for LSB)
+    lhalfadd=[XOR(A[0], B[0], curr_wire + 1), AND(A[0], B[0], curr_wire + 2)]
+    l+=lhalfadd
+    sum = [curr_wire + 1]
+    curr_wire+=2
+    gates+=2
+    # use chained full-adders for the other bits
+    for i in range(1, size):
+        curr_sum, curr_wire=ADD(A[i], B[i], curr_wire, l)
+        gates+=7
+        sum.append(curr_sum)
+    return sum, curr_wire, gates
+
+def BLKMEAN(A, zero, curr_wire, gates, l):
+    '''PRE: A a (2D)list of 16 lists(16 8-bit binary numbers sums of block pixels)
+        POST: l contains all the gates for finding the sum of the 16 number, returned are the 8b mean and updated curr-wire, gates'''
+
+    lvls = int(log(len(A), 2))
+    # use cascading approach to find the sum of all 16 numbers by adding two at a time => 4 "levels":
+    for lvl in range(1, lvls+1):
+        # on each level, calculate sum = list of pairwise sums
+        sum = []
+        for i in range(0, len(A), 2):
+            currsum, curr_wire, gates = BINADDER(A[i], A[i + 1], zero, curr_wire, gates, l)
+            sum.append(currsum)
+        # set A = sum since it contains the addends for the next "level"
+        A = sum
+    #A is now a list containing a single list(the 12-bit sum), chop off 4 LSB's to find mean (divide by 16)
+    return A[0][lvls::], curr_wire, gates
+
+def ALLMEANS(A, zero, curr_wire, gates, l):
+    '''PRE: A a (3D)list of 16 blocks (block is a list A descried in BLKMEAN)
+        POST: l contains all the gates for finding all 16 blocks' means and the mean of the means, ..
+            ..returned are the list containing the 16 block means and the main mean, updated curr_wire and gates'''
+    means = []
+    # for each block, calculate the mean and add it to the list
+    for i in A:
+        currmean, curr_wire, gates = BLKMEAN(i, zero, curr_wire, gates, l)
+        means.append(currmean)
+    # calculate the mean of the means and add it to the same list
+    prime_mean, curr_wire, gates = BLKMEAN(means, zero, curr_wire, gates, l)
+    means.append(prime_mean)
+    return means, curr_wire, gates
+
+def BLKHASH(M, one, curr_wire, gates, l):
+    '''PRE: M list of 17 means: M[0]..M[15] blk means, M[15] main mean
+        POST: l contains all gates for finding the 16-bit hash, returned are the list of hash labels, updated curr_wire and gates '''
+    size = len(M)
+    hash=[]
+    # for the block means, compare them to the main one and return the result (0 if less, 1 if gte), add the result to hash
+    for i in range(size-1):
+        curr_wire, gates = COMPARATOR(M[i], M[size-1], one, curr_wire, gates, l)
+        hash.append(curr_wire)
+    return hash, curr_wire, gates
---
+++ big_image_input_and_circuit_test.py
@@ -0,0 +1,137 @@
+import cv2
+import numpy as np #imported numpy because cv2 uses numpy data structures/data types like uint8
+
+# HASH fFOR THE 256x256 IMAGE:
+# 0010111111111100001111111111110000111111111111100011111111101111001011100111111100111111111111110011100111110111001100001111001100010000111100110001101101100111000111100000010100011100000001110001100000000111000110000000111100011000000000111111000000000000
+
+##replace "filename" input parameter with actual input name
+img = cv2.imread('img.jpg',0)
+
+block_side_length = 16
+img_side_length = 256
+last = img_side_length - block_side_length
+
+pi_list = []
+pi_list_export = []
+bin_pi_list = []
+bm_list = []
+
+# In this nested for loop, I am iterating through a 2-d matrix of 2-d matrices.
+# By dividing the image into blocks, each block itself is a 2d matrix of grayscale intensity values.
+
+init_row = 0
+while init_row <= last:
+
+    init_col = 0
+    while init_col <= last:
+
+        block_row = init_row
+        while block_row % 16 != 0 or block_row == init_row:
+
+            block_col = init_col
+            while block_col % 16 != 0 or block_col == init_col:
+
+
+                pixel_intensity = img[block_row, block_col] #find out the filetype of the image, it matters for this step. Might need to replace uchar with something else.
+                pi_list.append(pixel_intensity)
+                pi_list_export.append(pixel_intensity)
+                block_col+=1
+
+            block_row+=1
+
+        pi_sum = 0
+        for pi in pi_list:
+            pi_sum += pi
+
+        pi_list_length = len(pi_list)
+        block_mean = pi_sum // pi_list_length
+        bm_list.append(block_mean)
+        pi_list.clear()
+
+        init_col+=block_side_length
+
+    init_row+=block_side_length
+
+# This is to compute the median of the block mean values.
+# We're going to use the mean instead.
+# sorted_bm_list = bm_list.sort()
+# median = sorted_bm_list[len(sorted_bm_list)//2]
+
+bm_sum = 0
+for bm in bm_list:
+    bm_sum += bm
+
+bm_list_length = len(bm_list)
+mean_of_block_means = bm_sum // bm_list_length
+
+h_list = []
+
+for bm in bm_list:
+    if bm >= mean_of_block_means:
+        h_list.append('1')
+    else:
+        h_list.append('0')
+
+ret_hash = ''.join(h_list)
+print("image hash: ",ret_hash)
+
+#creates a list of the binary pixel intensities in (lsb to msb)least significant bit to most significant bit order
+#to ensure compatibility with circuit
+# bin_pi_list = []
+# for pi in pi_list:
+#     s = "{:08b}".format(pi)
+#     rev_s = s[::-1]
+#     bin_pi_list.append(rev_s)
+
+# ret_pi = ''.join(bin_pi_list)
+
+#old ret_pi returned pixel intensities in msb to lsb order
+ret_pi = ''.join(["{:08b}".format(pi) for pi in pi_list_export]) #creates boolean string from uint8 values in pixel intesnity list (pi_list)
+
+#write single-line output of image
+with open('bin_img.txt','w') as f:
+    f.write(ret_pi)
+    f.close()
+
+# the code below writes the bin_img.txt file to a format readable by libscapi's Yao test
+s = ''
+with open('bin_img.txt','r') as d:
+    s = d.read()
+d.close()
+print(len(s))
+
+with open('MPPHPartyOneInputs.txt','w') as x:
+    x.write('524288\n')
+    # for i in range(len(s)):
+    #     x.write(s[i])
+    #     x.write('\n')
+    for i in range(1,65537):
+        for j in range(8*i-1,8*(i-1)-1,-1):
+            # print(j)
+            x.write(s[j])
+            x.write('\n')
+    x.close()
+
+# TODOs
+#     1.[Later] Take input image and reduce to 256x256 (with cropping). This step, though it comes first in the
+#         algorithm, can be implemented after May 9th.
+#     2.[DONE] Convert image to grayscale.
+#     3.[DONE] Divide image into 256 row-major blocks.
+#     4.[DONE]For each of the blocks, calculate the mean intensity and put that mean into a vector of means
+#         after it's computed.
+#     5.[DONE] Sort the vector of means and find the median.
+#     6.Convert the median (of the block-means) into a binary number.
+#     7.somehow obtain the hash from the binary number (not too sure about this step)
+
+#     -implment mean instead of median
+#     -normalize the hash value
+
+# TODO(later)
+#     8. Address comments and questions about type to skeith.
+#     9. get filename from std::in, make a command line interface to this application.
+#     10. find out how to compile with the OpenCV lbrary.
+#     11. On about libscapi. Do Bon and Anastasia need me to incorporate their circuit into some c++ code, or will
+#     will they be using the example test script with a different circuit file. (The test script is in
+#     libraries/libscapi/samples/Yao
+
+#     Ask Rosario after 9th/10th to bring up the things.
